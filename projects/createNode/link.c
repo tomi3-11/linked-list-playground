74a4cd41442badf2c4d95ec3dd946461a93a6044
tom <didymusthomas6@gmail.com>
2026-06-27 16:49:05 +0300

    Update link code with 2 nodes

--- projects/createNode/link.c
+++ projects/createNode/link.c
@@ -9,14 +9,22 @@ struct Node {
 int main(){
     // Create Node
     struct Node *node;
+    struct Node *node1;
+
     node = malloc(sizeof(struct Node));
+    node1 = malloc(sizeof(struct Node));
 
     node->Data = 5;
-    node->next = NULL;
+    node->next = node1;
+    
+    node1->Data = 58;
+    node1->next = NULL;
     
     printf("The value is %d and pointer is %p\n", node->Data, (void *)node->next);
+    printf("The value is %d and pointer is %p\n", node1->Data, (void *)node1->next);
 
     free(node);
+    free(node1);
     return 0;
 
 }
